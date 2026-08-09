cf3e82c2d3e16d94e6f4cfd2094aba9bc675452b
Seemon-ch <seemonchaudhary99@gmail.com>
2026-08-09 23:45:31 +0530

    added js

---
+++ JS/home.js
@@ -0,0 +1,7 @@
+const startPractice = document.querySelector(".btn.btn-outline-primary");
+
+startPractice.addEventListener("click" , function(){
+    // console.log("button clicked")
+    // window.location.href="../practice.html";
+    
+});
