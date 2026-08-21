0f9ca5674ba9f3ea459a12b8f07ea74b1a8ba68e
Seemon-ch <seemonchaudhary99@gmail.com>
2026-08-21 16:27:03 +0530

    structured files

--- JS/home.js
+++
@@ -1,7 +0,0 @@
-const startPractice = document.querySelector(".btn.btn-outline-primary");
-
-startPractice.addEventListener("click" , function(){
-    // console.log("button clicked")
-    // window.location.href="../practice.html";
-    
-});
---
+++ public/JS/home.js
@@ -0,0 +1,9 @@
+const startPractice = document.querySelector(".btn.btn-outline-primary");
+const learnMore =document.querySelector(".btn.btn-primary")
+
+startPractice.addEventListener("click" , function(){
+    window.location.href = "../practice.html";   
+});
+learnMore.addEventListener("click" ,function(){
+    window.location.href ="../about.html";
+});
